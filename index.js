f7b5f8fd07b9b39a948f2d61e1245c44cb7aab3f
thomas <thomas@antmoves.com>
2020-02-25 22:23:29 +0200

    fix

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 Element.prototype.removeEventListeners = function (type) {
-    let listeners = getEventListeners(this)[type];
+    let listeners = window.getEventListeners(this)[type];
     if (listeners !== undefined && listeners.length > 0) {
         listeners.forEach(element => {
             this.removeEventListener(type, element.listener)
